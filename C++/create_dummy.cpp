1d7c1e996940ebd68ffc3dc4647d89a8341f6114
shimpei-iwata <shimpei.iwata@dena.com>
2014-10-31 18:46:27 +0900

    add dummy2

--- C++/create_dummy.cpp
+++ C++/create_dummy.cpp
@@ -8,8 +8,10 @@ using namespace std;
 
 int main()
 {
-    std::string index = "grade,class,name,english,math,japanese";
-    ofstream fout("dummy.csv");
+    //std::string index = "grade,class,name,english,math,japanese";
+    std::string index = "grade,class,name,history,sience";
+    //ofstream fout("dummy.csv");
+    ofstream fout("dummy2.csv");
     fout << index << endl;
 
     srand( (unsigned int)time(NULL));
@@ -41,25 +43,39 @@ int main()
         ss.clear(stringstream::goodbit);       
 
         // english
-        ss << rand()%100 + 1;
-        ss >> tmp;
-        fout << tmp << ",";
-        ss.str(""); // バッファをクリアする。
-        ss.clear(stringstream::goodbit);       
+//        ss << rand()%100 + 1;
+//        ss >> tmp;
+//        fout << tmp << ",";
+//        ss.str(""); // バッファをクリアする。
+//        ss.clear(stringstream::goodbit);       
 
         // math
+//        ss << rand()%100 + 1;
+//        ss >> tmp;
+//        fout << tmp << ",";
+//        ss.str(""); // バッファをクリアする。
+//        ss.clear(stringstream::goodbit);       
+//        
+//        // japanese
+//        ss << rand()%100 + 1;
+//        ss >> tmp;
+//        fout << tmp << endl;
+//        ss.str(""); // バッファをクリアする。
+//        ss.clear(stringstream::goodbit);       
+        // history
         ss << rand()%100 + 1;
         ss >> tmp;
         fout << tmp << ",";
         ss.str(""); // バッファをクリアする。
         ss.clear(stringstream::goodbit);       
         
-        // japanese
+        // sience
         ss << rand()%100 + 1;
         ss >> tmp;
         fout << tmp << endl;
         ss.str(""); // バッファをクリアする。
         ss.clear(stringstream::goodbit);       
+
     }
 
 
